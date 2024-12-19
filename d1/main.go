884e560bf6cc83006abedc9d0db000001ef19d74
austin <austin2118ace@gmail.com>
2024-12-19 00:19:49 -0500

    Add function to subtract indices. Need to further clean the input

--- d1/main.go
+++ d1/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -25,6 +27,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	indexVals := strings.Split(string(fileContents), " ")
-	fmt.Println(indexVals)
+	indexVals := strings.Split(string(fileContents), "   ")
+	_, err = subtractIndices(indexVals)
+	fmt.Println(err)
+}
+
+func prepIndices(indexVals []string) ([]string, error) {
+	return nil, nil
+}
+
+func subtractIndices(indexVals []string) ([]int, error) {
+
+	for i := 0; i < (len(indexVals) - 1); i += 2 {
+		value1 := indexVals[i]
+		value2 := indexVals[i+1]
+		int1, err := strconv.Atoi(value1)
+		int2, err2 := strconv.Atoi(value2)
+		if checkErr(err) || checkErr(err2) {
+			return nil, fmt.Errorf("unable to subtract indices")
+		}
+
+		diff := int1 - int2
+		posDiff := math.Abs(float64(diff))
+		fmt.Println(posDiff)
+	}
+	return nil, nil
 }
