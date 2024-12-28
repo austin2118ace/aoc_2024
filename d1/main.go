f007d64323cbef7adc5a4b300eceaea454badc2b
austin2118ace <austin2118ace@gmail.com>
2024-12-28 00:38:21 -0500

    split lists; sort; subtract; and sum

--- d1/main.go
+++ d1/main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -19,7 +20,7 @@ func checkErr(e error) bool {
 }
 
 func main() {
-	var inputString = "C:\\Projects\\aoc_2024\\d1\\input.txt"
+	var inputString = "input.txt"
 	var inputFilePath = filepath.FromSlash(inputString)
 	fileContents, err := os.ReadFile(inputFilePath)
 
@@ -27,29 +28,66 @@ func main() {
 		os.Exit(1)
 	}
 
-	indexVals := strings.Split(string(fileContents), "   ")
-	_, err = subtractIndices(indexVals)
-	fmt.Println(err)
+	indexVals := strings.Fields(string(fileContents))
+	list1, list2, err := splitIndices(indexVals)
+	if checkErr(err) {
+		os.Exit(1)
+	}
+	diffs, err := subtractIndices(list1, list2)
+
+	if checkErr(err) {
+		os.Exit(1)
+	}
+
+	total := sumDiffs(diffs)
+
+	fmt.Println("Total:", total)
 }
 
-func prepIndices(indexVals []string) ([]string, error) {
-	return nil, nil
+func sumDiffs(diffs []int) int {
+	var total = 0
+
+	for _, diff := range diffs {
+		total += diff
+	}
+	return total
 }
 
-func subtractIndices(indexVals []string) ([]int, error) {
+func subtractIndices(l1 []int, l2 []int) ([]int, error) {
+	if len(l1) != len(l2) {
+		return nil, fmt.Errorf("lengths not equal")
+	}
 
-	for i := 0; i < (len(indexVals) - 1); i += 2 {
-		value1 := indexVals[i]
-		value2 := indexVals[i+1]
-		int1, err := strconv.Atoi(value1)
-		int2, err2 := strconv.Atoi(value2)
+	slices.Sort(l1)
+	slices.Sort(l2)
+
+	diffs := make([]int, len(l1))
+	for i := 0; i < len(l1); i += 1 {
+		value1 := l1[i]
+		value2 := l2[i]
+
+		diff := value1 - value2
+		posDiff := int(math.Abs(float64(diff)))
+		diffs[i] = posDiff
+	}
+	return diffs, nil
+}
+
+func splitIndices(indexVals []string) ([]int, []int, error) {
+	l1 := make([]int, len(indexVals)/2)
+	l2 := make([]int, len(indexVals)/2)
+
+	for i := 0; i < (len(indexVals) / 2); i += 1 {
+		i1 := i * 2
+		i2 := i1 + 1
+		int1, err := strconv.Atoi(indexVals[i1])
+		int2, err2 := strconv.Atoi(indexVals[i2])
 		if checkErr(err) || checkErr(err2) {
-			return nil, fmt.Errorf("unable to subtract indices")
+			return nil, nil, fmt.Errorf("unable to convert indices")
 		}
 
-		diff := int1 - int2
-		posDiff := math.Abs(float64(diff))
-		fmt.Println(posDiff)
+		l1[i] = int1
+		l2[i] = int2
 	}
-	return nil, nil
+	return l1, l2, nil
 }
